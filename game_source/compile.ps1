99fd9b538b0f690adbc7bfb1542f012ba7a86c27
Federico Panarotto <104077886+federicopanarotto@users.noreply.github.com>
2023-01-04 20:21:15 +0100

    update script to compile the game

--- game_source/compile.ps1
+++ game_source/compile.ps1
@@ -1,1 +1,21 @@
-g++ main.cpp src/functions.cpp src/Cella.cpp src/AreaGioco.cpp src/menu.cpp -o battaglia_navale.exe -lncursesw
+$project_name = "Battaglia Navale"
+$author_name = "Federico Panarotto"
+$exe_name = "battaglia_navale"
+
+if(Test-Path -Path ../release) {
+
+    # Remove-Item ../release/battaglia_navale.exe
+    Remove-Item ../release -Recurse
+}
+mkdir ../release/ | Write-Output >> "cache"
+
+Write-Output "Building project $project_name dev. by $author_name"
+Write-Output "Compiling..."
+g++ main.cpp src/functions.cpp src/Cella.cpp src/AreaGioco.cpp src/menu.cpp -o $exe_name -lncursesw
+Write-Output "Project builded"
+Remove-Item cache
+
+# Set-Location ..
+$path_local = Get-Location
+Write-Output "EXE file is here: $path_local\release\"
+Move-Item battaglia_navale.exe ../release
