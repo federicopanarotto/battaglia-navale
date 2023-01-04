5a46f4e44a6cb933cb9a60804c7a7bce5a1ee2cf
Federico Panarotto <104077886+federicopanarotto@users.noreply.github.com>
2023-01-04 20:50:03 +0100

    update file to compile program

--- game_source/compile.ps1
+++ game_source/compile.ps1
@@ -1,21 +1,25 @@
+foreach($i in Get-ChildItem) {
+    if($i.Name -eq "game_source") {
+        Set-Location $i
+    }
+}
+
 $project_name = "Battaglia Navale"
 $author_name = "Federico Panarotto"
 $exe_name = "battaglia_navale"
 
 if(Test-Path -Path ../release) {
-
-    # Remove-Item ../release/battaglia_navale.exe
     Remove-Item ../release -Recurse
 }
-mkdir ../release/ | Write-Output >> "cache"
+Set-Location ..
+mkdir release/ | Write-Output >> "cache"
 
 Write-Output "Building project $project_name dev. by $author_name"
 Write-Output "Compiling..."
-g++ main.cpp src/functions.cpp src/Cella.cpp src/AreaGioco.cpp src/menu.cpp -o $exe_name -lncursesw
+g++ game_source/main.cpp game_source/src/functions.cpp game_source/src/Cella.cpp game_source/src/AreaGioco.cpp game_source/src/menu.cpp -o release/$exe_name -lncursesw
 Write-Output "Project builded"
-Remove-Item cache
 
-# Set-Location ..
 $path_local = Get-Location
 Write-Output "EXE file is here: $path_local\release\"
-Move-Item battaglia_navale.exe ../release
+
+Remove-Item cache
